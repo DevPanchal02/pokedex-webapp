7332b59da671f245a7b9fdcf94a388bbf08193f0
Dev Panchal <90882951+DevPanchal02@users.noreply.github.com>
2022-09-23 20:31:51 -0400

    Added images,text and styling to the website

---
+++ script.js
@@ -0,0 +1,122 @@
+let pokemon =  [
+    {
+        "number": "001",
+        "name": "Bulbasaur",
+        "type 1": "Grass",
+        "type 2": "Poison" 
+    },
+    {
+        "number": "002",
+        "name": "Ivysaur",
+        "type 1": "Grass",
+        "type 2": "Poison" 
+    },
+    {
+        "number": "003",
+        "name": "Venusaur",
+        "type 1": "Grass",
+        "type 2": "Poison" 
+    },
+    {
+        "number": "004",
+        "name": "Charmander",
+        "type 1": "Fire",
+        "type 2": "" 
+    },
+    {
+        "number": "005",
+        "name": "Charmeleon",
+        "type 1": "Fire",
+        "type 2": "" 
+    },
+    {
+        "number": "006",
+        "name": "Charizard",
+        "type 1": "Fire",
+        "type 2": "" 
+    },
+    {
+        "number": "007",
+        "name": "Squirtle",
+        "type 1": "Water",
+        "type 2": "" 
+    },
+    {
+        "number": "008",
+        "name": "Wartortle",
+        "type 1": "Water",
+        "type 2": "" 
+    },
+    {
+        "number": "009",
+        "name": "Blastoise",
+        "type 1": "Water",
+        "type 2": "" 
+    },
+    {
+        "number": "010",
+        "name": "Caterpie",
+        "type 1": "Bug",
+        "type 2": "" 
+    },
+    {
+        "number": "011",
+        "name": "Metapod",
+        "type 1": "Bug",
+        "type 2": "" 
+    },
+    {
+        "number": "012",
+        "name": "Buterfree",
+        "type 1": "Bug",
+        "type 2": "Flying" 
+    },
+    {
+        "number": "013",
+        "name": "Weedle",
+        "type 1": "Bug",
+        "type 2": "Poison" 
+    },
+    {
+        "number": "014",
+        "name": "Kakuna",
+        "type 1": "Bug",
+        "type 2": "Poison" 
+    },
+    {
+        "number": "015",
+        "name": "Beedrill",
+        "type 1": "Bug",
+        "type 2": "Poison" 
+    },
+    {
+        "number": "016",
+        "name": "Pidgey",
+        "type 1": "Normal",
+        "type 2": "Flying" 
+    },
+    {
+        "number": "017",
+        "name": "Pidgeotto",
+        "type 1": "Normal",
+        "type 2": "Flying" 
+    },
+    {
+        "number": "018",
+        "name": "Pidgeot",
+        "type 1": "Normal",
+        "type 2": "Flying" 
+    },
+    {
+        "number": "019",
+        "name": "Rattata",
+        "type 1": "Normal",
+        "type 2": "" 
+    },
+    {
+        "number": "Raticate",
+        "name": "Bulbasaur",
+        "type 1": "Normal",
+        "type 2": "" 
+    }
+    ]
